8120e9d41b061a36ba3c2f700d55e9af4bce3fef
HryhulevichDaniil <dan.grigulevich@gmail.com>
2026-06-07 15:08:49 +0300

    lose screen logic

---
+++ Assets/Game/Scripts/UI/Screens/LoseScreen.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+using Interfaces;
+
+namespace Screens
+{
+    public class LoseScreen : BaseScreen,ITickable
+    {
+        private Action _onRestart;
+
+        public void Init(Action onRestart)
+        {
+            _onRestart = onRestart;
+        }
+
+        public void Tick()
+        {
+            if (Keyboard.current.rKey.wasPressedThisFrame)
+            {
+                _onRestart?.Invoke();
+            }
+        }
+    }
+}
